7be6198d3b8b2365bde8dd0d12cbf183d8964e69
Irul <irul@Iruls-MacBook-Pro.local>
2020-06-30 15:05:06 +0700

    login page

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,33 +1,69 @@
 import React, {Component} from 'react';
-import {Text, View, Image, StyleSheet, Dimensions} from 'react-native';
+import {
+  Text,
+  TextInput,
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Alert,
+  KeyboardAvoidingView,
+} from 'react-native';
 
-const deviceWidth = Dimensions.get('screen').width;
-const deviceHeight = Dimensions.get('screen').height;
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
 
 import bg from '../assets/image/bg.jpeg';
 
 export default class Login extends Component {
   render() {
     return (
-      <View>
-        <View style={[loginStyle.fill, loginStyle.bgWrapper]}>
-          <Image style={loginStyle.bg} source={bg} />
-        </View>
-        {/* <View style={loginStyle.bgOverlay} /> */}
-        <View style={loginStyle.accent1}>
+      <KeyboardAvoidingView behavior={'position'} style={loginStyle.parent}>
+        <Image source={bg} style={loginStyle.accent1} />
+        <View style={loginStyle.accentOverlay} />
+        <View style={loginStyle.accent2}>
           <View style={loginStyle.logo}>
-            <Text style={loginStyle.logoItem}>R</Text>
+            <Text style={loginStyle.logoText}>R</Text>
           </View>
           <View style={loginStyle.brand}>
             <View style={loginStyle.brandWrapper}>
-              <Text style={loginStyle.brandText}>Rel</Text>
-              <View style={loginStyle.right}>
-                <Text style={[loginStyle.brandText]}>Ate</Text>
+              <View style={loginStyle.brandTextWrapper}>
+                <Text style={loginStyle.brandText}>Rel</Text>
+              </View>
+              <View style={loginStyle.brandTextWrapperRight}>
+                <Text style={loginStyle.brandText}>Ate</Text>
               </View>
             </View>
           </View>
         </View>
-      </View>
+        <View style={loginStyle.form}>
+          <View style={loginStyle.formCard}>
+            <View style={loginStyle.inputWrapper}>
+              <Text style={loginStyle.inputLabel}>Username</Text>
+              <TextInput
+                // placeholder={'Your Username'}
+                style={loginStyle.input}
+              />
+            </View>
+            <View style={loginStyle.inputWrapper}>
+              <Text style={loginStyle.inputLabel}>Password</Text>
+              <TextInput
+                // placeholder={'Your Password'}
+                style={loginStyle.input}
+              />
+            </View>
+            <TouchableOpacity
+              onPress={() => Alert.alert('Logging In...')}
+              style={loginStyle.submit}>
+              <Text style={loginStyle.submitText}>&gt;</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={loginStyle.link}>
+            <Text style={loginStyle.createAccount}>New Account</Text>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -35,50 +71,102 @@ export default class Login extends Component {
 const accentHeight = 250;
 
 const loginStyle = StyleSheet.create({
-  fill: {
+  parent: {
     flex: 1,
-  },
-  right: {
-    alignItems: 'flex-end',
+    position: 'relative',
   },
   accent1: {
-    backgroundColor: '#68896C',
-    height: accentHeight,
-    paddingLeft: 50,
-    paddingRight: 50,
-    paddingTop: 50,
-    width: '100%',
-    zIndex: 2,
-  },
-  bgOverlay: {
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    width: '100%',
-    height: '100%',
-  },
-  bg: {
-    height: deviceHeight,
+    position: 'absolute',
     width: deviceWidth,
+    height: deviceHeight,
+    zIndex: 0,
+  },
+  accentOverlay: {
+    position: 'absolute',
+    width: deviceWidth,
+    height: deviceHeight,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  accent2: {
+    height: accentHeight,
+    width: deviceWidth,
+    position: 'absolute',
+    // backgroundColor: '#62725A',
+    backgroundColor: '#1E2C1D',
+    zIndex: 2,
+    padding: 50,
   },
   logo: {
     alignItems: 'flex-end',
   },
-  logoItem: {
+  logoText: {
     fontSize: 80,
     fontWeight: 'bold',
-    color: '#EBEFDE',
+    color: '#FFF',
   },
   brand: {
-    position: 'relative',
+    width: 220,
   },
-  brandWrapper: {
-    // backgroundColor: 'black',
-    width: 200,
-    position: 'absolute',
-    zIndex: 99,
+  brandTextWrapperRight: {
+    alignItems: 'flex-end',
   },
   brandText: {
     fontSize: 60,
-    color: '#EBEFDE',
+    letterSpacing: 20,
+    fontWeight: 'bold',
     textTransform: 'uppercase',
+    color: '#FFF',
+  },
+  form: {
+    position: 'absolute',
+    zIndex: 4,
+    width: deviceWidth,
+    height: deviceHeight,
+    justifyContent: 'flex-end',
+  },
+  formCard: {
+    position: 'relative',
+    width: deviceWidth - 70,
+    height: 200,
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    paddingTop: 50,
+    paddingBottom: 50,
+  },
+  link: {
+    marginTop: 50,
+    marginBottom: 70,
+    paddingRight: 30,
+    paddingLeft: 30,
+    alignItems: 'baseline',
+  },
+  createAccount: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    letterSpacing: 5,
+  },
+  inputWrapper: {
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  inputLabel: {
+    textTransform: 'uppercase',
+    letterSpacing: 5,
+  },
+  submit: {
+    position: 'absolute',
+    width: 80,
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    bottom: -20,
+    right: -20,
+    backgroundColor: '#1E2C1D',
+  },
+  submitText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#FFF',
   },
 });
